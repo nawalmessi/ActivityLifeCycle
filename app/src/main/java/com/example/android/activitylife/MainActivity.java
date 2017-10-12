7e9f0a2955bc5068edd3ca5c9534a98632ce5ead
High Spec <nawal.messi1990@gmail.com>
2017-10-12 10:12:58 +0300

    added cpace

--- app/src/main/java/com/example/android/activitylife/MainActivity.java
+++ app/src/main/java/com/example/android/activitylife/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     MediaPlayer player;
     int position;
 
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
